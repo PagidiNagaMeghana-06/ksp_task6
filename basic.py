d8d1be7183c8cf44c827952ff026c66d04a208e8
PagidiNagaMeghana-06 <138564807+PagidiNagaMeghana-06@users.noreply.github.com>
2023-12-16 22:38:23 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,32 @@
+# This is a comment
+print("Hello World!");
+myvar = "Meghana"
+my_var = "Prerna"
+myvar1 = "Rahul"
+print(myvar)
+print(my_var)
+print(myvar1)
+x = 5
+print(type(x))
+y = true
+print(type(y))
+z = 12.8
+print(type(z))
+a = 23
+b = 30
+if b > a:
+    print("b is greater than a")
+else  :
+    print("a is greater than b")
+i = 1
+while i < 5;
+print(i)
+i += 1
+fruits = ["apple","banana","cherry"]
+for x in fruits:
+    print(x)
+def my_function ():
+    print("I am a good girl")
+my_function
+print(4-5)
+print(10+5)
